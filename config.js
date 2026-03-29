43055b590c8f557f9a905dc10616fa43f7f58ed2
Ben Slater <bas58@cam.ac.uk>
2026-03-29 10:29:09 +0100

    Add a feedback box to the final page

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const CONFIG = {
   S3_BASE_URL: 'https://test-experiment-data-storage.s3.eu-north-1.amazonaws.com',
+  LAMBDA_URL: 'https://ijxyyr92x3.execute-api.eu-north-1.amazonaws.com/test/telemetry',
 };
