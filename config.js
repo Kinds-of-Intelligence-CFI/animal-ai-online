ff389ecbcad85ee97e8e276cb93f1ba21d0ea21a
Ben Slater <bas58@cam.ac.uk>
2026-04-06 09:19:50 +0100

    Bug fix: Update the completion page to send to the csv upload lambda
    It used to send to the lifecycle state lambda

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
   S3_BASE_URL: 'https://test-experiment-data-storage.s3.eu-north-1.amazonaws.com',
-  LAMBDA_URL: 'https://ijxyyr92x3.execute-api.eu-north-1.amazonaws.com/test/telemetry',
+  LIFECYCLE_LAMBDA_URL: 'https://ijxyyr92x3.execute-api.eu-north-1.amazonaws.com/test/telemetry',
+  FEEDBACK_UPLOAD_LABMDA_URL: 'https://mjlo2ftcn3.execute-api.eu-north-1.amazonaws.com/Prod/telemetry'
 };
